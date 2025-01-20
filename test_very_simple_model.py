09d53b0f7c2323970057140d2d4f3ebd61798e0c
Sh0me <39559420+Asanaly@users.noreply.github.com>
2025-01-20 17:36:24 +0800

    Update test_very_simple_model.py

--- test_very_simple_model.py
+++ test_very_simple_model.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
 from torchvision import datasets, transforms
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -33,31 +31,20 @@ def train(model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % 10 == 0:  # Log interval
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
 
 def test(model, device, test_loader):
     model.eval()
-    test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print(f"Accuracy: {100. * correct / len(test_loader.dataset)}%")
 
 def main():
-    # Directly setting parameters for simplicity
     batch_size = 64
     test_batch_size = 1000
     epochs = 10
@@ -69,14 +56,14 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = DataLoader(
+    train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=batch_size, shuffle=True, **kwargs)
-    test_loader = DataLoader(
+    test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
@@ -86,11 +73,23 @@ def main():
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
-    for epoch in range(1, epochs + 1):
-        train(model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+    # Create CUDA Graph
+    if use_cuda:
+        # Setup the graph capturing
+        graph = torch.cuda.CUDAGraph()
+        data, target = next(iter(train_loader))
+        data, target = data.to(device), target.to(device)
+        
+        # Capture graph
+        with torch.cuda.graph(graph):
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
 
-    # Save the model
+        for epoch in range(1, epochs + 1):
+            train(model, device, train_loader, optimizer, epoch)
+            test(model, device, test_loader)
+
     torch.save(model.state_dict(), "mnist_cnn.pt")
 
 if __name__ == '__main__':
