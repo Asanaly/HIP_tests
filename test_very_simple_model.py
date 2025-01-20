81997940f2353db9f6e1a0b9e06fb1bd909ddf23
Sh0me <39559420+Asanaly@users.noreply.github.com>
2025-01-20 17:34:18 +0800

    Update test_very_simple_model.py

--- test_very_simple_model.py
+++ test_very_simple_model.py
@@ -1,55 +1,97 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-import time
-N, D_in, H, D_out = 640, 4096, 2048, 1024
-model = torch.nn.Sequential(torch.nn.Linear(D_in, H),
-                            torch.nn.Dropout(p=0.2),
-                            torch.nn.Linear(H, D_out),
-                            torch.nn.Dropout(p=0.1)).cuda()
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-# Placeholders used for capture
-static_input = torch.randn(N, D_in, device='cuda')
-static_target = torch.randn(N, D_out, device='cuda')
 
-# warmup
-# Uses static_input and static_target here for convenience,
-# but in a real setting, because the warmup includes optimizer.step()
-# you must use a few batches of real data.
-s = torch.cuda.Stream()
-s.wait_stream(torch.cuda.current_stream())
-with torch.cuda.stream(s):
-    for i in range(3):
-        optimizer.zero_grad(set_to_none=True)
-        y_pred = model(static_input)
-        loss = loss_fn(y_pred, static_target)
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-torch.cuda.current_stream().wait_stream(s)
+        if batch_idx % 10 == 0:  # Log interval
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
-# capture
-g = torch.cuda.CUDAGraph()
-# Sets grads to None before capture, so backward() will create
-# .grad attributes with allocations from the graph's private pool
-optimizer.zero_grad(set_to_none=True)
-with torch.cuda.graph(g):
-    static_y_pred = model(static_input)
-    static_loss = loss_fn(static_y_pred, static_target)
-    static_loss.backward()
-    optimizer.step()
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-real_inputs = [torch.rand_like(static_input) for _ in range(10)]
-real_targets = [torch.rand_like(static_target) for _ in range(10)]
+    test_loss /= len(test_loader.dataset)
 
-for data, target in zip(real_inputs, real_targets):
-    # Fills the graph's input memory with new data to compute on
-    static_input.copy_(data)
-    static_target.copy_(target)
-    # replay() includes forward, backward, and step.
-    # You don't even need to call optimizer.zero_grad() between iterations
-    # because the captured backward refills static .grad tensors in place.
-    g.replay()
-    # Params have been updated. static_y_pred, static_loss, and .grad
-    # attributes hold values from computing on this iteration's data.
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def main():
+    # Directly setting parameters for simplicity
+    batch_size = 64
+    test_batch_size = 1000
+    epochs = 10
+    lr = 0.01
+    momentum = 0.5
+    use_cuda = torch.cuda.is_available()
+
+    torch.manual_seed(1)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    test_loader = DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=test_batch_size, shuffle=False, **kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+
+    # Save the model
+    torch.save(model.state_dict(), "mnist_cnn.pt")
+
+if __name__ == '__main__':
+    main()
